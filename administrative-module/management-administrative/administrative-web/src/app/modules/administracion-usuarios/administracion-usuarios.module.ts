e2852911999e65002c0a5024883d2f099cca9509
iSeth1998 <duvanbradbrid@gmail.com>
2020-02-01 23:18:34 -0500

    Inicializacion del back

--- administrative-module/management-administrative/administrative-web/src/app/modules/administracion-usuarios/administracion-usuarios.module.ts
+++ administrative-module/management-administrative/administrative-web/src/app/modules/administracion-usuarios/administracion-usuarios.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListComponent } from './usuarios/list/list.component';
 import { AdministracionUsuariosRoutingModule } from './administracion-usuarios-routing.module';
-import { TipoUComponent } from './tipo-usuarios/tipo-u/tipo-u.component';
 import { TipoUsuariosComponent } from './tipo-usuarios/tipo-usuarios.component';
 
 
 
 @NgModule({
-  declarations: [ListComponent, TipoUComponent, TipoUsuariosComponent],
+  declarations: [ListComponent, TipoUsuariosComponent],
   imports: [
     CommonModule,
     AdministracionUsuariosRoutingModule
