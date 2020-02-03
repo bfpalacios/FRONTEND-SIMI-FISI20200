19d109e673e3eff2dfb5e2ef1a693d15f0b0516e
JoelTC <57535643+JoelTC@users.noreply.github.com>
2020-02-02 23:49:15 -0500

    matricula-online

--- operative-module/matricula-online/matricula-web/src/app/modules/periodo-academico/periodo-academico-routing.module.ts
+++ operative-module/matricula-online/matricula-web/src/app/modules/periodo-academico/periodo-academico-routing.module.ts
@@ -1,19 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {MatriculaOnlineComponent} from './matricula-online/matricula-online.component';
 
-/* const routes: Routes = [{
+const routes: Routes = [{
   path: '',
-  component: MainComponent,
+  component: MatriculaOnlineComponent,
 },
-{
+/*{
   path: 'feed',
   component: FeedComponent,
-},
+},*/
 {
   path: '**',
   redirectTo: ''
 },
-]; */
+];
 
 @NgModule({
   imports: [/* RouterModule.forChild(routes) */],
