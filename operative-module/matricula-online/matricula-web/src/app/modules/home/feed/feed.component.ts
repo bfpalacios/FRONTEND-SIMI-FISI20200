5cb593e8591b0ff5b9ed4e1a1c37ca1125a17382
iSeth1998 <duvanbradbrid@gmail.com>
2020-02-09 13:33:28 -0500

    Feed

--- operative-module/matricula-online/matricula-web/src/app/modules/home/feed/feed.component.ts
+++ operative-module/matricula-online/matricula-web/src/app/modules/home/feed/feed.component.ts
@@ -7,7 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedComponent implements OnInit {
 
-  constructor() { }
+  public photos: any[];
+  constructor() {
+    this.photos = [];
+    this.photos.push(
+      {image: 'assets/logo-language/ingles.svg'},
+      {image: 'assets/logo-language/coreano.svg'},
+      {image: 'assets/logo-language/aleman.svg'},
+      {image: 'assets/logo-language/quechua.svg'},
+      {image: 'assets/logo-language/italiano.svg'},
+      {image: 'assets/logo-language/portugues.svg'},
+      {image: 'assets/logo-language/japones.svg'},
+      {image: 'assets/logo-language/frances.svg'});
+
+   }
 
   ngOnInit() {
   }
--- operative-module/matricula-online/matricula-web/src/app/modules/home/home.module.ts
+++ operative-module/matricula-online/matricula-web/src/app/modules/home/home.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FeedComponent } from './feed/feed.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { NewsComponent } from './feed/news/news.component';
 
 
 
 @NgModule({
-  declarations: [FeedComponent],
+  declarations: [FeedComponent, NewsComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
