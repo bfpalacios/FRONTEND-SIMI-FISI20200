9d7026f252e3e248d2a479f2f0c4bfed6744caad
Brandon Duvan Saenz Falcon <bsaenz@hiper.com.pe>
2020-02-10 18:32:33 -0500

    CiberOink

---
+++ operative-module/login-operative/src/main/java/pe/edu/ceid/simi/operative/Application.java
@@ -0,0 +1,17 @@
+package pe.edu.ceid.simi.operative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class Application extends SpringBootServletInitializer{
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
