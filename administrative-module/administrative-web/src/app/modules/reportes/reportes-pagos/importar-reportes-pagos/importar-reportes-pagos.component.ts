2e9701e1c7363c091ca66159b6ebf02e0dfad4be
iSeth1998 <duvanbradbrid@gmail.com>
2020-02-13 19:45:02 -0500

    Reporte de Pagos - Importar Pagos realizados en el banco

---
+++ administrative-module/administrative-web/src/app/modules/reportes/reportes-pagos/importar-reportes-pagos/importar-reportes-pagos.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import * as XLSX from 'xlsx';
+import { UploaderComponent } from '@syncfusion/ej2-angular-inputs';
+import { Router } from '@angular/router';
+import { Path } from 'src/app/infrastructure/constans/Path';
+@Component({
+  selector: 'app-importar-reportes-pagos',
+  templateUrl: './importar-reportes-pagos.component.html',
+  styleUrls: ['./importar-reportes-pagos.component.css']
+})
+export class ImportarReportesPagosComponent implements OnInit {
+
+  @ViewChild('defaultupload', {static: true}) uploadObj: UploaderComponent;
+
+  public fileLoaded: boolean;
+  public pagos: any[];
+  public load: boolean;
+  public loading: string;
+  constructor(
+    private router: Router
+  ) {
+    this.load = false;
+    this.loading = Path.loading;
+    this.fileLoaded = false;
+   }
+
+  ngOnInit() {
+  }
+
+  public onFileRemove(args): void {
+    args.cancel = true;
+  }
+
+  public parseExcel(file: any) {
+    this.load = true;
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const data = (e.target).result;
+      const workbook = XLSX.read(data, {
+        type: 'binary'
+      });
+      workbook.SheetNames.forEach(( (sheetName) => {
+        const xlRowObject = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName]);
+        this.pagos = xlRowObject;
+        this.fileLoaded = true;
+        this.load = false;
+      }).bind(this), this);
+    };
+
+    reader.onerror = (ex) => {
+      console.log(ex);
+    };
+    reader.readAsBinaryString(file);
+  }
+
+  public onSuccess(args: any): void {
+    const files = args.target.files;
+    this.parseExcel(files[0]);
+  }
+
+  public reportPagos() {
+    this.router.navigate(['reportes/pagos']).then();
+  }
+
+  public guardar() {
+    this.router.navigate(['reportes/pagos']).then();
+  }
+
+}
