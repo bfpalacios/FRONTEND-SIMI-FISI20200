d9adb8330d707af46b30f0a8ecd8c4057d18761b
iSeth1998 <duvanbradbrid@gmail.com>
2020-02-27 20:13:25 -0500

    MATRICULA ONLINE - MATRICULAR Y VERIFICAR MATRICULA Y FINALIZAR MATRICULA

--- operative-module/matricula-web/src/app/modules/home/feed/feed.component.ts
+++ operative-module/matricula-web/src/app/modules/home/feed/feed.component.ts
@@ -1,18 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Output, EventEmitter, DoCheck } from '@angular/core';
+import { DatosUsuarioFeedComponent } from '../datos-usuario-feed/datos-usuario-feed.component';
 
 @Component({
   selector: 'app-feed',
   templateUrl: './feed.component.html',
   styleUrls: ['./feed.component.css']
 })
-export class FeedComponent implements OnInit {
+export class FeedComponent implements OnInit, DoCheck {
 
+  public load: boolean;
   public userDates: any;
-  
+
   constructor() {
-    this.userDates = new Object;
-   }
+    this.load = true;
+    this.userDates = new Object();
+  }
 
   ngOnInit() {
+    localStorage.setItem('SIMI-LOAD-HOME', 'true');
+  }
+
+  ngDoCheck() {
+    const l = localStorage.getItem('SIMI-LOAD-HOME');
+    this.load = l === 'true' ? true : false;
+    console.log(this.load);
   }
 }
--- operative-module/matricula-web/src/app/modules/profile/profile.module.ts
+++ operative-module/matricula-web/src/app/modules/profile/profile.module.ts
@@ -4,11 +4,12 @@ import { ProfileRoutingModule } from './profile-routing.module';
 import { HistorialAcademicoComponent } from './historial-academico/historial-academico.component';
 import { InformacionAcademicaComponent } from './informacion-academica/informacion-academica.component';
 import { CuentaUsuarioComponent } from './cuenta-usuario/cuenta-usuario.component';
+import { HistorialDatosAlumnoComponent } from './historial-academica/historial-datos-alumno/historial-datos-alumno.component';
 
 
 
 @NgModule({
-  declarations: [HistorialAcademicoComponent, InformacionAcademicaComponent, CuentaUsuarioComponent],
+  declarations: [HistorialAcademicoComponent, InformacionAcademicaComponent, CuentaUsuarioComponent, HistorialDatosAlumnoComponent],
   imports: [
     CommonModule,
     ProfileRoutingModule
