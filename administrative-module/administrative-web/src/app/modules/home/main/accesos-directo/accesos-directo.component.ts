ea7cd3652e522bf3d15e028c33e03991aa2c5758
NatalyGVS <nataly.vasquez9718@gmail.com>
2020-03-02 17:44:36 -0500

    Menu actual

--- administrative-module/administrative-web/src/app/modules/home/main/accesos-directo/accesos-directo.component.ts
+++ administrative-module/administrative-web/src/app/modules/home/main/accesos-directo/accesos-directo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-accesos-directo',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccesosDirectoComponent implements OnInit {
 
-  constructor() { }
+  constructor( private router: Router  ) { }
 
   ngOnInit() {
+  }
+
+  matricula() {
+  
+    this.router.navigate(['administracionMatricula/matricula/crear']).then();
+  }
+  pc() {
+  
+    this.router.navigate(['periodoAcademico/programacionCursos']).then();
+  }
+  estudiantes() {
+  
+    this.router.navigate(['administracionInstitucional/estudiantes']).then();
+  }
+  docentes() {
+  
+    this.router.navigate(['administracionInstitucional/docentes']).then();
+  }
+  cicloAcademico() {
+  
+    this.router.navigate(['periodoAcademico/cicloAcademico']).then();
+  }
+  institucionales() {
+  
+    this.router.navigate(['reportes/institucionales']).then();
+  }
+  reportes() {
+  
+    this.router.navigate(['reportes/institucionales']).then();
   }
 
 }
